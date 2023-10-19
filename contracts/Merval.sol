af4c28460e1c8c628a9473bcf18c7145a158202a
matiasratcliffe <mati.ratcliffe@gmail.com>
2023-10-19 16:55:57 -0300

    formatting changes

--- contracts/Merval.sol
+++ contracts/Merval.sol
@@ -5,39 +5,18 @@ pragma solidity ^0.8.9;
 import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "./Dates.sol" ;
- //SON LOTES DE 10 ACCIONES
+//SON LOTES DE 10 ACCIONES
 
 
- //TODO
+//TODO
 //Manejar timestamp y ejercicio
- //TODO: Crear ticker, ejemplo GGALC271023
- //Tenes todo en remix
+//TODO: Crear ticker, ejemplo GGALC271023
+//Tenes todo en remix
  
 
 contract Merval {
-    IERC20 public usdtToken;
-    function removeByIndex(uint[] storage array, uint index) internal {
-        if (index >= array.length) return;
-
-        for (uint i = index; i < array.length-1; i++){
-            array[i] = array[i+1];
-        }
-        array.pop();
-    }
-    BokkyPooBahsDateTimeLibraryEdited DatesLibrary= new BokkyPooBahsDateTimeLibraryEdited();
-
-    uint timestamp;
-    function timeStampManager() public returns (uint)  {
-        uint _timestamp=block.timestamp;
-        if(_timestamp>timestamp+2592000){
-            timestamp=_timestamp+2592000;
-        }
-        return timestamp;
-    }
-
-
     struct Option {
-        string tikcer;
+        string ticker;
         uint optionID;
         address writer;
         address owner;
@@ -48,22 +27,14 @@ contract Merval {
         bool isCall;
         bool isInTheMarket;
         bool isExercised;
-        
     }
+
     struct User {
         uint userID;
         address userAddress;
         uint[] Writtenoptions;
         uint[] ownedOptions;
     }
-
-    mapping(address => User) public users;
-    uint256 public userCount;
-  
-    Option[] public options;
-    uint public mervalIndex=783; //Se remplaza por CHAINLINK
-    
-    uint usdtvalue = 10**18;
 
     event OptionCreated(
         address indexed Lanzador,
@@ -74,22 +45,52 @@ contract Merval {
         bool isCall
     );
 
-        function createUser(address _address)p ublic {
+    IERC20 public usdtToken;
+
+    mapping(address => User) public users;
+    uint256 public userCount;
+  
+    Option[] public options;
+    uint public mervalIndex = 783; //Se remplaza por CHAINLINK
+    
+    uint usdtvalue = 10**18;
+
+    function removeByIndex(uint[] storage array, uint index) internal {
+        if (index >= array.length) return;
+
+        for (uint i = index; i < array.length-1; i++){
+            array[i] = array[i+1];
+        }
+        array.pop();
+    }
+    BokkyPooBahsDateTimeLibraryEdited DatesLibrary = new BokkyPooBahsDateTimeLibraryEdited();
+
+    uint timestamp;
+    function timeStampManager() public returns (uint)  {
+        uint _timestamp=block.timestamp;
+        if(_timestamp>timestamp+2592000){
+            timestamp=_timestamp+2592000;
+        }
+        return timestamp;
+    }
+
+    function createUser(address _address) public {
         User memory newUser = User(userCount, _address, new uint[](0), new uint[](0));
         users[_address] = newUser;
-        userCount+=userCount;
+        userCount += 1;
+    }
 
-        }
-        function ensureUserExists(address _address) internal {
+    function ensureUserExists(address _address) internal {
         if (users[_address].userAddress == address(0)) {
             createUser(_address);
         }
     }
-        function getUserData(address _userID) public view returns (User memory) {
-        return( users[_userID]);
 
-        }
-        function createOption(uint _strikePrice, uint _price, bool _isCall) public  {
+    function getUserData(address _userID) public view returns (User memory) {
+        return( users[_userID]);
+    }
+
+    function createOption(uint _strikePrice, uint _price, bool _isCall) public  {
         ensureUserExists(msg.sender);
         uint collateral=((mervalIndex * 30)/2)*usdtvalue;
 
@@ -111,6 +112,7 @@ contract Merval {
         emit OptionCreated(msg.sender, _strikePrice, expiration, _price, collateral, _isCall);
         //SEND TO COMPUND
     }
+
     function buyOption(uint _optionID) public payable {
         ensureUserExists(msg.sender);
         uint batchPrice=usdtvalue*options[_optionID].price*10;
@@ -132,16 +134,16 @@ contract Merval {
 
         users[msg.sender].ownedOptions.push(_optionID);
     }
+
     function sellOption(uint _optionID) public {
         ensureUserExists(msg.sender);
 
         require(options[_optionID].owner == msg.sender, "You are not the owner of this option");
         require(options[_optionID].isInTheMarket == false, "This option is already in the market");
         options[_optionID].isInTheMarket = true;
-            }
+    }
 
-
-    function canceSellOrder(uint256 _optionID) public{
+    function canceSellOrder(uint256 _optionID) public {
         require(options[_optionID].writer == msg.sender, "You are not the writer of this option");
         require(options[_optionID].isInTheMarket == true, "This option is not in the market");
         require(options[_optionID].isExercised == false, "This option is already exercised");
@@ -160,29 +162,26 @@ contract Merval {
         if (options[_optionID].isCall == true) {
             address Writer = options[_optionID].writer;
             uint256 callGain = options[_optionID].strikePrice > mervalIndex ? (options[_optionID].strikePrice-mervalIndex)*10 : 0;     
-            if(options[_optionID].collateral>callGain){
-            //payable(msg.sender).transfer(callGain);-
-            //Recuperamos la guita del pool de compound  y  lo mandamos a ambos miembros
-            usdtToken.transfer(msg.sender, callGain);
-            usdtToken.transfer(Writer, options[_optionID].collateral-callGain);
-            }
-            else{
+            if(options[_optionID].collateral>callGain) {
+                //payable(msg.sender).transfer(callGain);-
+                //Recuperamos la guita del pool de compound  y  lo mandamos a ambos miembros
+                usdtToken.transfer(msg.sender, callGain);
+                usdtToken.transfer(Writer, options[_optionID].collateral-callGain);
+            } else {
                 payable(msg.sender).transfer(options[_optionID].collateral);
                 usdtToken.transfer(Writer, options[_optionID].collateral);
             }
-
         } else {
             uint256 putGain = options[_optionID].strikePrice < mervalIndex ? (mervalIndex-options[_optionID].strikePrice)*10 :0;
             address Writer = options[_optionID].writer;
 
             if(options[_optionID].collateral>putGain){
-            payable(msg.sender).transfer(putGain);
-            usdtToken.transfer(msg.sender, putGain);
-            usdtToken.transfer(Writer, options[_optionID].collateral-putGain);
-
+                payable(msg.sender).transfer(putGain);
+                usdtToken.transfer(msg.sender, putGain);
+                usdtToken.transfer(Writer, options[_optionID].collateral-putGain);
+            } else {
+                usdtToken.transfer(Writer, options[_optionID].collateral);
             }
-            else{
-            usdtToken.transfer(Writer, options[_optionID].collateral);
-            }
-            
-        }}}
+        }
+    }
+}
