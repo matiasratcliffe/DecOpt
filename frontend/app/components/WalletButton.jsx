46c5ee32c9da5b996ee516c56a49f2b45db219ba
matiasratcliffe <mati.ratcliffe@gmail.com>
2023-10-21 02:10:43 -0300

    save changes

--- frontend/app/components/WalletButton.jsx
+++ frontend/app/components/WalletButton.jsx
@@ -5,7 +5,7 @@ import { useEffect, useState } from "react";
 export default function WalletButton(props) {
     const [isConnected, setIsConnected] = [props.isConnected, props.setIsConnected];
     const [hasMetamask, setHasMetamask] = useState(false);
-    const [signer, setSigner] = useState(undefined);
+    const setSigner = props.setSigner;
   
     useEffect(() => {
       if (typeof window.ethereum !== "undefined") {
@@ -14,34 +14,32 @@ export default function WalletButton(props) {
     })
   
     async function connect() {
-        if (typeof window.ethereum !== "undefined") {
-          try {
-            await ethereum.request({ method: "eth_requestAccounts" });
-            setIsConnected(true);
-            const provider = new ethers.providers.Web3Provider(window.ethereum);
-            setSigner(provider.getSigner());
-          } catch (e) {
-            console.log(e);
-          }
-        } else {
-          setIsConnected(false);
+      if (typeof window.ethereum !== "undefined") {
+        try {
+          await ethereum.request({ method: "eth_requestAccounts" });
+          setIsConnected(true);
+          const provider = new ethers.providers.Web3Provider(window.ethereum);
+          setSigner(provider.getSigner());
+        } catch (e) {
+          console.log(e);
         }
+      } else {
+        setIsConnected(false);
       }
+    }
   
     return (
       <main>
         <div>
           {hasMetamask ? (
             isConnected ? (
-              "Connected! "
+              "Connected!"
             ) : (
               <button onClick={() => connect()}>Connect</button>
             )
           ) : (
             "Please install metamask"
           )}
-  
-          {isConnected ? <button onClick={() => execute()}>Execute</button> : ""}
         </div>
       </main>
     )
--- frontend/app/page.js
+++ frontend/app/page.js
@@ -6,13 +6,116 @@ import { useEffect, useState } from "react";
 
 export default function Home() {
   const [isConnected, setIsConnected] = useState(false);
-  const [hasMetamask, setHasMetamask] = useState(false);
   const [signer, setSigner] = useState(undefined);
+
+  async function execute() {
+    if (typeof window.ethereum !== "undefined") {
+      const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+      const abi = [
+        {
+          inputs: [
+            {
+              internalType: "string",
+              name: "_name",
+              type: "string",
+            },
+            {
+              internalType: "uint256",
+              name: "_favoriteNumber",
+              type: "uint256",
+            },
+          ],
+          name: "addPerson",
+          outputs: [],
+          stateMutability: "nonpayable",
+          type: "function",
+        },
+        {
+          inputs: [
+            {
+              internalType: "string",
+              name: "",
+              type: "string",
+            },
+          ],
+          name: "nameToFavoriteNumber",
+          outputs: [
+            {
+              internalType: "uint256",
+              name: "",
+              type: "uint256",
+            },
+          ],
+          stateMutability: "view",
+          type: "function",
+        },
+        {
+          inputs: [
+            {
+              internalType: "uint256",
+              name: "",
+              type: "uint256",
+            },
+          ],
+          name: "people",
+          outputs: [
+            {
+              internalType: "uint256",
+              name: "favoriteNumber",
+              type: "uint256",
+            },
+            {
+              internalType: "string",
+              name: "name",
+              type: "string",
+            },
+          ],
+          stateMutability: "view",
+          type: "function",
+        },
+        {
+          inputs: [],
+          name: "retrieve",
+          outputs: [
+            {
+              internalType: "uint256",
+              name: "",
+              type: "uint256",
+            },
+          ],
+          stateMutability: "view",
+          type: "function",
+        },
+        {
+          inputs: [
+            {
+              internalType: "uint256",
+              name: "_favoriteNumber",
+              type: "uint256",
+            },
+          ],
+          name: "store",
+          outputs: [],
+          stateMutability: "nonpayable",
+          type: "function",
+        },
+      ];
+      const contract = new ethers.Contract(contractAddress, abi, signer);
+      try {
+        await contract.store(42);
+      } catch (error) {
+        console.log(error);
+      }
+    } else {
+      console.log("Please install MetaMask");
+    }
+  }
 
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <WalletButton isConnected={isConnected} setIsConnected={setIsConnected}></WalletButton>
+        <WalletButton isConnected={isConnected} setIsConnected={setIsConnected} setSigner={setSigner}></WalletButton>
+        {isConnected ? <button onClick={() => execute()}>Execute</button> : ""}
       </div>
     </main>
   )
